252fdd3cb15a18b5853a43222a425399ba696334
Mitchell Schellenger <mschellenger@osisoft.com>
2019-02-27 09:24:57 -0800

    updating copyright

--- Tutorials/CSharp_Sds/API/Message.cs
+++ Tutorials/CSharp_Sds/API/Message.cs
@@ -1,4 +1,4 @@
-﻿//Copyright 2018 OSIsoft, LLC
+﻿//Copyright 2018-2019 OSIsoft, LLC
 //
 //Licensed under the Apache License, Version 2.0 (the "License");
 //you may not use this file except in compliance with the License.
